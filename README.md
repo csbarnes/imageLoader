4e2655e512afc39fec041464a1e3a7b40d20db95
csbarnes <barnes.csb@gmail.com>
2012-12-15 14:58:30 -0500

    initial upload

---
+++ README.md
@@ -0,0 +1,4 @@
+imageLoader
+=========
+
+jquery plugin that provides automatic image uploading for sequential images
---
+++ jquery.imageLoader.js
@@ -0,0 +1,71 @@
+(function($){
+	$.fn.daPlugin = function(options) {
+		var defaults = {
+			digits: 3,
+			format: ".png",
+			numOfImages: 10,
+			path: "images/",
+			prefix: "",
+			postfix: ""
+		};
+
+		var options = $.extend(defaults, options);
+		options.path = options.path + options.prefix;
+		options.postfix = options.postfix + options.format;
+	
+		return this.each(function() {
+			var x = 1;
+			obj = $(this);
+		
+			while(x <= options.numOfImages){
+				if(x == 1)
+					obj.append('<li><img alt="" class="current thumb" src="' + options.path + padZeros(x, options.digits) + options.postfix + '" /></li>');
+				else
+					obj.append('<li><img alt="" class="thumb" src="' + options.path + padZeros(x, options.digits) + options.postfix + '" /></li>');
+					
+				x++;
+			}
+		});
+	};
+})(jQuery);
+
+(function($){
+	$.fn.daPlugin2 = function(options) {
+		var defaults = {
+			digits: 3,
+			format: ".png",
+			numOfImages: 10,
+			path: "images/",
+			prefix: "",
+			postfix: ""
+		};
+
+		var options = $.extend(defaults, options);
+		options.path = options.path + options.prefix;
+		options.postfix = options.postfix + options.format;
+	
+		return this.each(function() {
+			var x = 1;
+			obj = $(this);
+		
+			while(x <= options.numOfImages){
+				if(x == 1)
+					obj.append('<img alt="" src="' + options.path + padZeros(x, options.digits) + options.postfix + '" />');
+				else
+					obj.append('<img alt="" src="' + options.path + padZeros(x, options.digits) + options.postfix + '" />');
+					
+				x++;
+			}
+		});
+	};
+})(jQuery);
+
+function padZeros(num, len){
+	var result = '' + num;
+
+	while(result.length < len){
+		result = '0' + result;
+	}
+
+	return result;
+}
